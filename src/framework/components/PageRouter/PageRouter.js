d1217b96e754c1ff81faf6e4566e02545df66441
Alexander Pustovalov <apustovalov@gmail.com>
2020-02-26 22:37:18 +0200

    Fix targets property in the PageRouter

--- src/framework/components/PageRouter/PageRouter.js
+++ src/framework/components/PageRouter/PageRouter.js
@@ -6,7 +6,7 @@ import PageComposition from '../PageComposition';
 import WarningComponent from '../WarningComponent';
 
 const PageRouter = (props) => {
-  const { routes, pages, userComponents, history, actionSequences } = props;
+  const { routes, pages, userComponents, history, actionSequences, targets } = props;
   if (!routes || routes.length === 0) {
     return (<WarningComponent message="Application does not have pages."/>);
   }
@@ -26,6 +26,7 @@ const PageRouter = (props) => {
                   userComponents={userComponents}
                   componentsTree={get(pages, route.pageName, {})}
                   actionSequences={actionSequences}
+                  targets={targets}
                   routePath={route.path}
                 />
             }
@@ -41,6 +42,7 @@ const PageRouter = (props) => {
                     userComponents={userComponents}
                     componentsTree={get(pages, noMatchRoute.pageName, {})}
                     actionSequences={actionSequences}
+                    targets={targets}
                     routePath={noMatchRoute.path}
                   />
               }
